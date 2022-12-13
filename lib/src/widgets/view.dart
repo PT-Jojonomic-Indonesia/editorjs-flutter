72e2a68a725cc8a824bbf70df9d2b1d0086c1381
adityaaugusta <aditya.augusta@jojonomic.com>
2022-12-13 15:39:40 +0700

    improve viewer

--- lib/src/widgets/view.dart
+++ lib/src/widgets/view.dart
@@ -1,168 +1,37 @@
 import 'dart:convert';
 
+import 'package:editorjs_flutter/src/model/EditorJSBlock.dart';
 import 'package:flutter/material.dart';
 import 'package:editorjs_flutter/src/model/EditorJSData.dart';
-import 'package:editorjs_flutter/src/model/EditorJSViewStyles.dart';
-import 'package:editorjs_flutter/src/model/EditorJSCSSTag.dart';
 import 'package:flutter_html/flutter_html.dart';
 
-class EditorJSView extends StatefulWidget {
-  final String? editorJSData;
-  final String? styles;
+typedef EditorJSComponentBuilder = Widget Function(
+  BuildContext context,
+  EditorJSBlock block,
+);
 
-  const EditorJSView({Key? key, this.editorJSData, this.styles})
-      : super(key: key);
+class EditorJSView extends StatefulWidget {
+  const EditorJSView({Key? key, required this.editorJSData}) : super(key: key);
+
+  final String editorJSData;
 
   @override
   EditorJSViewState createState() => EditorJSViewState();
 }
 
 class EditorJSViewState extends State<EditorJSView> {
-  String? data;
-  late EditorJSData dataObject;
-  late EditorJSViewStyles styles;
   final List<Widget> items = <Widget>[];
-  late Map<String, Style> customStyleMap;
+  EditorJSData? dataObject;
 
   @override
   void initState() {
     super.initState();
-
-    setState(
-          () {
-        dataObject = EditorJSData.fromJson(jsonDecode(widget.editorJSData!));
-        styles = EditorJSViewStyles.fromJson(jsonDecode(widget.styles!));
-
-        customStyleMap = generateStylemap(styles.cssTags!);
-
-        dataObject.blocks!.forEach(
-              (element) {
-            double levelFontSize = 16;
-
-            switch (element.data!.level) {
-              case 1:
-                levelFontSize = 32;
-                break;
-              case 2:
-                levelFontSize = 24;
-                break;
-              case 3:
-                levelFontSize = 16;
-                break;
-              case 4:
-                levelFontSize = 12;
-                break;
-              case 5:
-                levelFontSize = 10;
-                break;
-              case 6:
-                levelFontSize = 8;
-                break;
-            }
-
-            switch (element.type) {
-              case "header":
-                items.add(Row(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Expanded(child: Text(
-                        element.data!.text!,
-                        style: TextStyle(
-                            fontSize: levelFontSize,
-                            fontWeight: (element.data!.level! <= 3)
-                                ? FontWeight.bold
-                                : FontWeight.normal),
-                      ),),
-                    ]));
-                break;
-              case "paragraph":
-                items.add(Html(
-                  data: element.data!.text,
-                  style: customStyleMap,
-                ));
-                break;
-              case "list":
-                String bullet = "\u2022 ";
-                String? style = element.data!.style;
-                int counter = 1;
-
-                element.data!.items!.forEach(
-                      (element) {
-                    if (style == 'ordered') {
-                      bullet = counter.toString();
-                      items.add(
-                        Row(children: [
-                          Expanded(
-                            child: Container(
-                                child: Html(
-                                  data: bullet + element,
-                                  style: customStyleMap,
-                                )),
-                          )
-                        ]),
-                      );
-                      counter++;
-                    } else {
-                      items.add(
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                                child: Container(
-                                  child: Html(
-                                      data: bullet + element,
-                                      style: customStyleMap),
-                                )
-                            )
-                          ],
-                        ),
-                      );
-                    }
-                  },
-                );
-                break;
-              case "delimiter":
-                items.add(Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text('***', style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold), textAlign: TextAlign.center,)
-                      // Expanded(child: Divider(color: Colors.grey))
-                    ]));
-                break;
-              case "image":
-                items.add(Image.network(element.data!.file!.url!));
-                break;
-              default:
-                items.add(SizedBox.shrink());
-            }
-            items.add(const SizedBox(height: 10));
-          },
-        );
-      },
-    );
+    _buildBlocks();
   }
 
-  Map<String, Style> generateStylemap(List<EditorJSCSSTag> styles) {
-    Map<String, Style> map = <String, Style>{};
-
-    styles.forEach(
-          (element) {
-        map.putIfAbsent(
-            element.tag.toString(),
-                () =>
-                Style(
-                    backgroundColor: (element.backgroundColor != null)
-                        ? getColor(element.backgroundColor!)
-                        : null,
-                    color:
-                    (element.color != null) ? getColor(element.color!) : null,
-                    padding: (element.padding != null)
-                        ? EdgeInsets.all(element.padding!)
-                        : null));
-      },
-    );
-
-    return map;
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: items);
   }
 
   Color getColor(String hexColor) {
@@ -170,8 +39,104 @@ class EditorJSViewState extends State<EditorJSView> {
     return Color(int.parse('$hexCode', radix: 16));
   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Column(children: items);
+  void _buildBlocks() {
+    setState(() {
+      final defaultStyleMap = {
+        'body': Style(margin: Margins.zero),
+        'code': Style(
+          backgroundColor: getColor('#33ff0000'),
+          color: getColor('#ffff0000'),
+          padding: EdgeInsets.all(5.0),
+        ),
+        'mark': Style(
+          backgroundColor: getColor('#ffffff00'),
+          padding: EdgeInsets.all(5.0),
+        ),
+      };
+      dataObject = EditorJSData.fromJson(jsonDecode(widget.editorJSData));
+      dataObject?.blocks?.forEach((element) {
+        switch (element.type) {
+          case "header":
+            items.add(Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: Html(
+                    data: "<h${element.data!.level!}>" +
+                        element.data!.text! +
+                        "</h${element.data!.level!}>",
+                    style: defaultStyleMap,
+                  ),
+                ),
+              ],
+            ));
+            break;
+          case "paragraph":
+            items.add(Html(
+              data: element.data!.text,
+              style: defaultStyleMap,
+            ));
+            break;
+          case "list":
+            String bullet = "\u2022 ";
+            String? style = element.data!.style;
+            int counter = 1;
+            element.data!.items!.forEach((element) {
+              if (style == 'ordered') {
+                bullet = counter.toString();
+                items.add(Row(children: [
+                  Expanded(
+                    child: Html(
+                      data: bullet + element,
+                      style: defaultStyleMap,
+                    ),
+                  )
+                ]));
+                counter++;
+              } else {
+                items.add(Row(
+                  children: <Widget>[
+                    Expanded(
+                        child: Html(
+                      data: bullet + element,
+                      style: defaultStyleMap,
+                    )),
+                  ],
+                ));
+              }
+            });
+            break;
+          case "delimiter":
+            items.add(Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  '***',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                )
+                // Expanded(child: Divider(color: Colors.grey))
+              ],
+            ));
+            break;
+          case "image":
+            items.add(Image.network(element.data!.file!.url!));
+            break;
+          default:
+            items.add(Container(
+              width: double.infinity,
+              height: 64.0,
+              color: Colors.black12,
+              child: Center(child: Text('Unsupported Block')),
+            ));
+        }
+        //Add space for each blocks
+        items.add(const SizedBox(height: 10));
+      });
+    });
   }
 }
